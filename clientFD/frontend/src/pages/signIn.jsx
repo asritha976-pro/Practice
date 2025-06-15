eaba6660b38bbc145d0f91bf29d92e56b754c092
asritha976-pro <asrithaavula623@gmail.com>
2025-06-15 20:43:26 +0530

    SignIn page on progress

---
+++ clientFD/frontend/src/pages/signIn.jsx
@@ -0,0 +1,20 @@
+export default function signIn()
+{
+    return(
+        <>
+        <form>
+            <div id="signInContainer" className = "signInContainer">
+                <div className = "userName-Ip">
+                    <label for="userName">
+                        <input type="email" placeholder="Email" required /> <br/>
+                    </label>
+                </div>
+            
+            </div>
+        </form>
+        
+        </>
+    )
+        
+    
+}
