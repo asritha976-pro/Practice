9651525fec8e51aaf6574e71f33220cd235df4b4
asritha976-pro <asrithaavula623@gmail.com>
2025-06-19 11:50:37 +0530

    backend on process

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,8 @@ require('dotenv').config();
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000")
-})
+app.use(express.json());
+
 
 // Connect to database
  const MONGODB_URI = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_ENDPOINT}/${process.env.DATABASE_NAME}`
@@ -18,3 +17,109 @@ app.listen(3000,()=>{
     console.log('MongoDB Connection Error:',error.message)
     process.exit(1);
  })
+
+
+ // Expense mode;
+
+ const expenseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    amount:{
+        type: Number,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    date:{
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+ });
+
+ // Expense model
+
+ const Expenses = mongoose.model('expenses',expenseSchema)
+
+ // Routes
+ // api to get all expenses
+
+ app.get('/expenses',async(req,res) => {
+    try{
+        const expenses = await Expenses.find();
+        return res.status(200).send({expenses:expenses})
+    }catch(error){
+        console.error(`Error fetching all expense`,error.message)
+        return res.status(500).send({error:`Error fetching all Expenses`})
+    }
+ });
+ // api to get expenses by id
+
+ app.get('/expenses/:id',async(req,res) => {
+    const id = req.params.id;
+    try{
+        const expense = await Expenses.findById(id);
+        if(!expense){
+            return res.status(404).send({error:'Expense not found'})
+        }
+        return res.status(200).send({expense:expense})
+    }catch(error){
+         console.error('Error fetching todo:',error.message)
+        return res.status(500).send({error:'Error fetching expense'})
+
+    }
+ });
+ // api to create expense
+
+ app.post('/expenses',async(req,res) => {
+    const {title,amount,category,date} = req.body;
+    try{
+        const newExpense = new Expenses({title, amount, category, date})
+        const savedExpense = await newExpense.save()
+        return res.status(201).send({newExpense:savedExpense})
+    }catch(error){
+        console.log('Error creating expense:',error.message)
+        return res.status(400).send({error:'Error creating expense'})
+    }
+ });
+
+ // api to update expense by id
+ app.patch('/expenses/:id',async(req,res) => {
+    const id = req.params.id
+    try{
+        const updatedExpenses = await Expenses.findByIdAndUpdate(id,req.body,{new:true})
+        if(!updatedExpenses){
+            return res.status(404).send({error:'Expense not found'})
+        }
+        return res.status(200).send({updatedExpenses:updatedExpenses})
+    }catch(error){
+        console.error('Error updating expense')
+        return res.status(400).send({error:'Error updating expense '})
+
+    }
+ })
+
+ // api to delete expense by id
+
+ app.delete('/expenses/:id',async(req,res) =>{
+    const id = req.params.id
+    try{
+        const deleteExpense = await Expenses.findByIdAndDelete(id,{new:true})
+        if(!deleteExpense){
+            return res.status(404).send({error:'Expense not found'})
+        }
+        return res.status(200).send({deleteExpense:deleteExpense})
+    }catch(error){
+        console.error('Error deleting Expense',error.message);
+        return res.status(500).send({error:"Error deleting Expense"})
+    }
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server running on port 3000")
+})
