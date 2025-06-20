fbd6b90422aaba60677ffe1596ebeb876c5d6533
asritha976-pro <asrithaavula623@gmail.com>
2025-06-20 22:52:02 +0530

    Initial Balnce feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,3 +9,20 @@ exports.getAllUsers = async(req,res) => {
         return res.status(500).send({error:'Error fetching all users'});
     }
 }
+
+exports.setInitialBalance = async(req,res) => {
+    try{
+        const userId = req.user.id;
+        const {balance} = req.body;
+
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            {balance},
+            {new : true}
+        );
+
+        return res.status(200).send({message:"Your Initial balance", balance: updatedUser.balance});
+    }catch(error){
+        return res.status(400).send({error: "Error initialising balance"});
+    }
+}
